0439f0c162ce672e3becda9da4e0ef8ccca95880
Tobias <81333097+tofrad@users.noreply.github.com>
2026-06-27 16:54:08 +0200

    implemented menu ui

--- include/Menu.hpp
+++ include/Menu.hpp
@@ -25,53 +25,100 @@ class Menu
 		void close();
 
 		void displayGUI();
-		 
 
 	private:
 
 		ProgramCallbacks menu_callbacks;
 
-		//Program* program = nullptr;
+		LayoutManager& layout_manager = LayoutManager::GetInstance();
 
 		void syncToProgram();
 
 		void requestStateChange(ProgramState newState) const;
 
-	/*	void generatorRequest();
-
-		void solverRequest();
-
-		void WindowRequest();*/
-
 		MenuState state = MenuState::CLOSED;
+		void UpdateRectValues();
 
-		//GUI Variables-------------------------------------------------------		
+		Vector2 anchor01 = { 0, 0 };
+		//Menu Rects #######################################################################################
+        Rectangle ListViewGeneration{ anchor01.x + 32, anchor01.y + 32, 344, 384 };
+        Rectangle ListViewSolving{ anchor01.x + 776, anchor01.y + 32, 344, 384 };
+        Rectangle CheckBoxWeights{ anchor01.x + 408, anchor01.y + 144, 24, 24 };
+        Rectangle SliderSize{ anchor01.x + 408, anchor01.y + 112, 320, 16 };
+        Rectangle GeneratorCanvas{ anchor01.x + 32, anchor01.y + 464, 656, 368 };
+        Rectangle SolvingCanvas{ anchor01.x + 776, anchor01.y + 464, 656, 368 };
+        Rectangle SpinnerWeightCnt{ anchor01.x + 408, anchor01.y + 216, 120, 24 };
+        Rectangle SpinnerMinWeight{ anchor01.x + 408, anchor01.y + 304, 120, 24 };
+        Rectangle TextBoxWeightCnt{ anchor01.x + 408, anchor01.y + 184, 120, 24 };
+        Rectangle TextBoxMinWeight{ anchor01.x + 408, anchor01.y + 272, 120, 24 };
+        Rectangle LineWeightsSeparate{ anchor01.x + 408, anchor01.y + 248, 344, 16 };
+        Rectangle TextBoxSize{ anchor01.x + 408, anchor01.y + 72, 120, 24 };
+        Rectangle TextBoxMaxWeight{ anchor01.x + 544, anchor01.y + 272, 120, 24 };
+        Rectangle SpinnerMaxWeight{ anchor01.x + 544, anchor01.y + 304, 120, 24 };
+        Rectangle GenerateButton{ anchor01.x + 408, anchor01.y + 360, 120, 24 };
+        Rectangle SolveGenButton{ anchor01.x + 1152, anchor01.y + 360, 120, 24 };
+        Rectangle ButtonExit{ anchor01.x + 1504, anchor01.y + 16, 80, 32 };
+        Rectangle Rect_PlayEdit{ anchor01.x + 1424, anchor01.y + 264, 160, 136 };
+        Rectangle ButtonEditor{ anchor01.x + 1448, anchor01.y + 296, 120, 24 };
+        Rectangle ButtonPlayer{ anchor01.x + 1448, anchor01.y + 336, 120, 24 };
+        Rectangle LinePlayerEditor{ anchor01.x + 1448, anchor01.y + 320, 120, 16 };
 
-		float dropdown_height = 40;
-		float dropdown_length = 250;
-		float dropdown_thick = 30;
+		Rectangle Scaled_ListViewGeneration = layout_manager.ScaleRect(ListViewGeneration);
+		Rectangle Scaled_ListViewSolving = layout_manager.ScaleRect(ListViewSolving);
+		Rectangle Scaled_CheckBoxWeights = layout_manager.ScaleRect(CheckBoxWeights);
+		Rectangle Scaled_SliderSize = layout_manager.ScaleRect(SliderSize);
+		Rectangle Scaled_GeneratorCanvas = layout_manager.ScaleRect(GeneratorCanvas);
+		Rectangle Scaled_SolvingCanvas = layout_manager.ScaleRect(SolvingCanvas);
+		Rectangle Scaled_SpinnerWeightCnt = layout_manager.ScaleRect(SpinnerWeightCnt);
+		Rectangle Scaled_SpinnerMinWeight = layout_manager.ScaleRect(SpinnerMinWeight);
+		Rectangle Scaled_TextBoxWeightCnt = layout_manager.ScaleRect(TextBoxWeightCnt);
+		Rectangle Scaled_TextBoxMinWeight = layout_manager.ScaleRect(TextBoxMinWeight);
+		Rectangle Scaled_LineWeightsSeparate = layout_manager.ScaleRect(LineWeightsSeparate);
+		Rectangle Scaled_TextBoxSize = layout_manager.ScaleRect(TextBoxSize);
+		Rectangle Scaled_TextBoxMaxWeight = layout_manager.ScaleRect(TextBoxMaxWeight);
+		Rectangle Scaled_SpinnerMaxWeight = layout_manager.ScaleRect(SpinnerMaxWeight);
+		Rectangle Scaled_GenerateButton = layout_manager.ScaleRect(GenerateButton);
+		Rectangle Scaled_SolveGenButton = layout_manager.ScaleRect(SolveGenButton);
+		Rectangle Scaled_ButtonExit = layout_manager.ScaleRect(ButtonExit);
+		Rectangle Scaled_Rect_PlayEdit = layout_manager.ScaleRect(Rect_PlayEdit);
+		Rectangle Scaled_ButtonEditor = layout_manager.ScaleRect(ButtonEditor);
+		Rectangle Scaled_ButtonPlayer = layout_manager.ScaleRect(ButtonPlayer);
+		Rectangle Scaled_LinePlayerEditor = layout_manager.ScaleRect(LinePlayerEditor);
+
+		//GUI Variables-------------------------------------------------------
+		int ListViewGenerationScrollIndex = 0;
+		int ListViewGenerationActive = 0;
+
+		int ListViewSolvingScrollIndex = 0;
+		int ListViewSolvingActive = 0;
+
+		bool CheckBoxWeightsChecked = false;
+
+		float SliderSizeValue = 0.0f;
+		char TextBoxSizeText[8] = "";
+
+		int SpinnerWeightCntValue = 0;
+		int SpinnerMinWeightValue = 0;
+		int SpinnerMaxWeightValue = 0;
 
 		int Maze_GUI = 0;
 		int Path_GUI = 0;
-		int Window_GUI = 0;
 
 		bool MazeEdit = false;
 		bool PathEdit = false;
-		bool WindowEdit = false;
 
-		bool Generate_Button = false;
-		bool Solve_Button = false;
-		bool Exit_Button = false;
+		bool Generate_Button_pressed = false;
+		bool Solve_Button_pressed = false;
+		bool Exit_Button_pressed = false;
+		bool Editor_Button_pressed = false;
+		bool Player_Button_pressed = false;
 
 		//Values from GUI Input-----------------------------------------------
 
 		//Maze-------------------------------------
 		int MazeSize = 50;
 		GenerationMethod MazeMethod = GenerationMethod::REC_BACKTRACKING;
-
 		//Solver-----------------------------------
-		SolvingMethod PathMethod = SolvingMethod::SM_BFS;		
-
-		Screensize WindowSize = Screensize::FHD;
+		SolvingMethod PathMethod = SolvingMethod::SM_BFS;
 };
 
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -36,117 +36,71 @@ void Menu::close()
 
 void Menu::displayGUI()
 {
-    ClearBackground(GRAY);
+    layout_manager.UpdateScale();
+    UpdateRectValues();
 
-    int window_height;
-    int window_width;
+    ClearBackground(LIGHTGRAY);
+    //draw ui ###############################################################################################
+    //Maze List View
+    GuiListView(Scaled_ListViewGeneration,"Recursive Backtracking\nKruskal\nHunt & Kill", &ListViewGenerationScrollIndex, &Maze_GUI);
+    //Path List View
+    GuiListView(Scaled_ListViewSolving, "DFS\nBFS", &ListViewSolvingScrollIndex, &Path_GUI);
 
-    switch (menu_callbacks.getWindowSize()) {
-    case UHD:
-        window_height = 1440;
-        window_width = 2560;
-        break;
-    case FHD:
-        window_height = 1080;
-        window_width = 1920;
-        break;
-    case WSXGA:
-        window_height = 900;
-        window_width = 1600;
-        break;
-    case SMALL:
-        window_height = 540;
-        window_width = 960;
-        break;
-    default:
-        window_height = 540;
-        window_width = 960;
-        break;
-    }
+    GuiCheckBox(Scaled_CheckBoxWeights, "with weights", &CheckBoxWeightsChecked);
 
-    constexpr float Label_offset = 10;
+    GuiSlider(Scaled_SliderSize,"10", "100", &SliderSizeValue, 10, 100);
+    MazeSize = static_cast<int>(SliderSizeValue);
 
-    const float Labelwidth = (window_width - (4*Label_offset)) / 3;
+    snprintf(TextBoxSizeText, sizeof(TextBoxSizeText), "%d", MazeSize);
+    GuiTextBox(Scaled_TextBoxSize, TextBoxSizeText, DEFAULT, false);
 
+    GuiSpinner(Scaled_SpinnerWeightCnt, "", &SpinnerWeightCntValue, 1, 50, false);
+    GuiTextBox(Scaled_TextBoxWeightCnt, "number of weights", DEFAULT, false);
+
+    GuiSpinner(Scaled_SpinnerMinWeight, "", &SpinnerMinWeightValue, 1, 50, false);
+    GuiTextBox(Scaled_TextBoxMinWeight, "min weights val", DEFAULT, false);
+
+    GuiSpinner(Scaled_SpinnerMaxWeight, "", &SpinnerMaxWeightValue, 1, 50, false);
+    GuiTextBox(Scaled_TextBoxMaxWeight, "max weights val", DEFAULT, false);
+
+    GuiLine(Scaled_LineWeightsSeparate, "");
+
+    if (GuiButton(Scaled_GenerateButton, "Generate")){Generate_Button_pressed = true;}
+    if (GuiButton(Scaled_SolveGenButton, "Solve")){Solve_Button_pressed = true;}
+    if (GuiButton(Scaled_ButtonExit, "Exit")){Exit_Button_pressed = true;}
+
+    DrawRectangleLinesEx(Scaled_Rect_PlayEdit, 3, BLACK);
+    if (GuiButton(Scaled_ButtonEditor, "Editor")){Editor_Button_pressed = true;};
+    if (GuiButton(Scaled_ButtonPlayer, "Player")){Player_Button_pressed = true;};
+    GuiLine(Scaled_LinePlayerEditor, "");
+
+    DrawRectangleRec(Scaled_GeneratorCanvas, SKYBLUE);
+    DrawRectangleRec(Scaled_SolvingCanvas, BROWN);
+
+    // resolve actions ######################################################################################
     if (state == MenuState::OPEN) {
-        constexpr float Label_height = 30;
-        constexpr float Labelpos_y = 10;
-
-        float x = Label_offset;
-        constexpr float y = Labelpos_y;
-        
-        //GuiListView(Rectangle{ x, y + Label_height + 1, Labelwidth, dropdown_thick}, "BACKTRACKING;KRUSKAL;HUNTANDKILL;CUSTOM", &test , &Maze_GUI);
-        // 
-        //Terminate Section ##############################################################################################################################################################
-        if (GuiButton(Rectangle{window_width - Label_offset - Labelwidth, window_height - Label_offset - 2*Label_height, Labelwidth, Label_height }, "Exit Program")) {
-
-            Exit_Button = true;
-        }
-
-        //Generator Section ##############################################################################################################################################################
-
-        if (GuiButton(Rectangle{ x, y + 3 * Label_height, Labelwidth, Label_height }, "Generate")) {
-
-            Generate_Button = true;
-        }
-
-        GuiLabel(Rectangle{ x, y, Labelwidth, Label_height }, "Maze Generator");
-        if (GuiDropdownBox(Rectangle{ x, y + Label_height + 1, Labelwidth, dropdown_thick }, "BACKTRACKING;KRUSKAL;HUNTANDKILL;CUSTOM", &Maze_GUI, MazeEdit)) {
-
-            MazeEdit = !MazeEdit;
-            MazeMethod = static_cast<GenerationMethod>(Maze_GUI);
-        }
-
-        //Solver Section #################################################################################################################################################################
-
-        x = x + Labelwidth + Label_offset;
-
-        if (GuiButton(Rectangle{ x, y + 3 * Label_height, Labelwidth, Label_height }, "Solve")) {
-
-            Solve_Button = true;
-        }
-        
-        GuiLabel(Rectangle{ x , y, Labelwidth, Label_height }, "Path Solver");
-        if (GuiDropdownBox(Rectangle{ x, y + Label_height + 1, Labelwidth, dropdown_thick }, "BFS;DFS", &Path_GUI, PathEdit)) {
-
-            PathEdit = !PathEdit;
-            PathMethod = static_cast<SolvingMethod>(Path_GUI);
-        }
-
-        //Window Section ################################################################################################################################################################
-
-        x = x + Labelwidth + Label_offset;
-
-        GuiLabel(Rectangle{ x , y, Labelwidth, Label_height }, "Window Size");
-        if (GuiDropdownBox(Rectangle{ x, y + Label_height + 1, Labelwidth, dropdown_thick }, "UHD;FHD;WSXGA;SMALL", &Window_GUI, WindowEdit)) {
-
-            WindowEdit = !WindowEdit;
-            WindowSize = static_cast<Screensize>(Window_GUI);
-            menu_callbacks.onWindowRequest(WindowSize);
-        }
 
         //Resolve Actions here
-        if (Generate_Button) {
-
+        if (Generate_Button_pressed) {
             menu_callbacks.onGenerateRequest(MazeSize, MazeMethod);
-            //menu_callbacks.onSolveRequest(PathMethod);
-
-            Generate_Button = false;
+            Generate_Button_pressed = false;
         }
-
-        if (Solve_Button) {
+        if (Solve_Button_pressed) {
             menu_callbacks.onSolveRequest(PathMethod);
-
-            Solve_Button = false;
+            Solve_Button_pressed = false;
         }
-
-        if (Exit_Button) {
-
+        if (Exit_Button_pressed) {
             requestStateChange(STOPPED);
-
-            Exit_Button = false;
+            Exit_Button_pressed = false;
         }
-
+        if (Editor_Button_pressed) {
+            requestStateChange(EDITING);
+            Editor_Button_pressed = false;
+        }
+        if (Player_Button_pressed) {
+            requestStateChange(PLAYER);
+            Player_Button_pressed = false;
+        }
     }
 }
 
@@ -161,15 +115,37 @@ void Menu::syncToProgram()
     if (menu_callbacks.getSolver) {
         PathMethod = menu_callbacks.getSolver();
     }
-    if (menu_callbacks.getWindowSize) {
-        WindowSize = menu_callbacks.getWindowSize();
-    }
+
     Maze_GUI = static_cast<int>(MazeMethod);
     Path_GUI = static_cast<int>(PathMethod);
-    Window_GUI = static_cast<int>(WindowSize);
 }
 
 void Menu::requestStateChange(const ProgramState newState) const
 {
     menu_callbacks.onStateRequest(newState);
+}
+
+void Menu::UpdateRectValues()
+{
+    Scaled_ListViewGeneration = layout_manager.ScaleRect(ListViewGeneration);
+    Scaled_ListViewSolving = layout_manager.ScaleRect(ListViewSolving);
+    Scaled_CheckBoxWeights = layout_manager.ScaleRect(CheckBoxWeights);
+    Scaled_SliderSize = layout_manager.ScaleRect(SliderSize);
+    Scaled_GeneratorCanvas = layout_manager.ScaleRect(GeneratorCanvas);
+    Scaled_SolvingCanvas = layout_manager.ScaleRect(SolvingCanvas);
+    Scaled_SpinnerWeightCnt = layout_manager.ScaleRect(SpinnerWeightCnt);
+    Scaled_SpinnerMinWeight = layout_manager.ScaleRect(SpinnerMinWeight);
+    Scaled_TextBoxWeightCnt = layout_manager.ScaleRect(TextBoxWeightCnt);
+    Scaled_TextBoxMinWeight = layout_manager.ScaleRect(TextBoxMinWeight);
+    Scaled_LineWeightsSeparate = layout_manager.ScaleRect(LineWeightsSeparate);
+    Scaled_TextBoxSize = layout_manager.ScaleRect(TextBoxSize);
+    Scaled_TextBoxMaxWeight = layout_manager.ScaleRect(TextBoxMaxWeight);
+    Scaled_SpinnerMaxWeight = layout_manager.ScaleRect(SpinnerMaxWeight);
+    Scaled_GenerateButton = layout_manager.ScaleRect(GenerateButton);
+    Scaled_SolveGenButton = layout_manager.ScaleRect(SolveGenButton);
+    Scaled_ButtonExit = layout_manager.ScaleRect(ButtonExit);
+    Scaled_Rect_PlayEdit = layout_manager.ScaleRect(Rect_PlayEdit);
+    Scaled_ButtonEditor = layout_manager.ScaleRect(ButtonEditor);
+    Scaled_ButtonPlayer = layout_manager.ScaleRect(ButtonPlayer);
+    Scaled_LinePlayerEditor = layout_manager.ScaleRect(LinePlayerEditor);
 }
